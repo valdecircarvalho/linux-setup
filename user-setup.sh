df4730b07ef69e39477916a2523c643d76309e77
Valdecir Carvalho (Val) <valdecir.carvalho@outlook.com>
2024-02-22 11:46:51 -0300

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -6,16 +6,14 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# Define o nome de usuário
+# Define o nome de usuário e a URL da chave SSH pública
 username="valdecir"
-
-# URL de onde a chave SSH pública será baixada
 ssh_key_url="https://raw.githubusercontent.com/valdecircarvalho/linux-setup/main/id_rsa.pub"
 
-# Cria o usuário sem senha
+# Cria o usuário sem definir uma senha
 useradd -m -s /bin/bash "$username"
 
-# Verifica se o comando anterior foi bem-sucedido
+# Verifica se o usuário foi criado com sucesso
 if [ $? -eq 0 ]; then
     echo "Usuário $username foi criado com sucesso."
 else
@@ -23,21 +21,16 @@ else
     exit 1
 fi
 
-# Baixa a chave SSH pública
+# Baixa a chave SSH pública e configura as permissões
 mkdir -p /home/"$username"/.ssh
 curl -s "$ssh_key_url" > /home/"$username"/.ssh/authorized_keys
-
-# Verifica se a chave foi baixada com sucesso
-if [ $? -eq 0 ]; then
-    echo "Chave SSH pública baixada com sucesso."
-else
-    echo "Falha ao baixar a chave SSH pública."
-    exit 1
-fi
-
-# Define as permissões adequadas
 chown -R "$username":"$username" /home/"$username"/.ssh
 chmod 700 /home/"$username"/.ssh
 chmod 600 /home/"$username"/.ssh/authorized_keys
 
-echo "Configuração concluída com sucesso para o usuário $username."
+# Configura o usuário para executar comandos sudo sem senha
+echo "$username ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/"$username"
+chmod 0440 /etc/sudoers.d/"$username"
+
+echo "Chave SSH adicionada com sucesso para o usuário $username."
+echo "Usuário $username configurado para executar comandos sudo sem senha."
